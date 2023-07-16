4be78a298179605c23bafbc1326ce7a72b9d074f
milanmdev <milanmdev@gmail.com>
2023-07-15 19:07:07 -0500

    update build

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ fi
 
 # Build the production image
 echo "Building the production image..."
-docker rmi -f ghcr.io/milanmdev/threadsfix && docker build . --platform linux/amd64 -t ghcr.io/milanmdev/threadsfix
+docker rmi -f ghcr.io/milanmdev/fixthreads && docker build . --platform linux/amd64 -t ghcr.io/milanmdev/fixthreads
 
 # Push the image to the GitHub Container Registry
 echo "Pushing the image to the GitHub Container Registry..."
-docker push ghcr.io/milanmdev/threadsfix
+docker push ghcr.io/milanmdev/fixthreads
